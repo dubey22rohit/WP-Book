8a08211100665d407c536bf42dc7cfd8845fe5ac
Rohit Dubey <dubey22rohit@gmail.com>
2020-10-05 12:33:40 +0530

    Inital Commit

---
+++ wp-book.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+    Plugin Name: WP-Book
+    Description: Creates a plugin named WP Book (wp-book),Creates a custom hierarchical taxonomy Book Category,Creates a custom non-hierarchical taxonomy Book Tag,Creates a custom meta box to save book meta information like Author Name, Price, Publisher, Year, Edition, URL, etc.
+    Author:Rohit Dubey
+    Version : 1.0
+*/
+?>
